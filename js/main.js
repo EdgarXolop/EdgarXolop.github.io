ff4823f6090e0701cf25fa9bfd987dc2db5afb8b
Edgar Xolop <edgarxoix@gmail.com>
2017-06-13 00:30:30 -0600

    Vue resource and route for menu

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
-const LINKS = [
-		{path: "about", label: "About"},
-		{path: "portafolio", label: "Portafolio"},
-		{path: "contact", label: "Contact"},
-		{path: "certifications", label: "Certifications"}
-	];
+Vue.use(VueRouter);
+
+const ROUTES = [
+	{path: "/about", label: "About", component : {template : "<about></about>"}},
+	{path: "/portafolio", label: "Portafolio", component : {template : "<about></about>"}},
+	{path: "/contact", label: "Contact", component : {template : "<about></about>"}},
+	{path: "/certifications", label: "Certifications", component : {template : "<about></about>"}}
+];
+
+const router = new VueRouter({
+	routes : ROUTES
+});
 
 var main_location = 0;
 
+Vue.component('about', function (resolve, reject) {
+	Vue.http.get('components/about.component').then(
+		response => {
+			resolve({
+				template: response.body
+			});
+		},
+		response => {
+			resolve({
+				template: "<h1>This page is not available.</h1>"
+			});
+		}
+	);
+});
+
+
 var menu = new Vue({
+	router,
 	el: '#menu',
 	data: {
 		toggleIsActive: false,
-		routes: LINKS,
-		location: {path: "", label: ""}
+		routes: ROUTES,
+		location: ROUTES[main_location]
 	},
 	methods:{
 		toggleButton: function(){
@@ -21,8 +44,5 @@ var menu = new Vue({
 		setRoute: function(route){
 			this.location = route;
 		}
-	},
-	mounted: function(){
-		this.location = this.routes[main_location];
 	}
 });
